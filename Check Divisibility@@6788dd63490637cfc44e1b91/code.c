9772844d3ab19389c847b6e04163775981aba84f
amanpundir2 <aman0255.becse24@chitkara.edu.in>
2025-02-17 00:31:58 +0530

    Solution to Check Divisibility

--- Check Divisibility@@6788dd63490637cfc44e1b91/code.c
+++ Check Divisibility@@6788dd63490637cfc44e1b91/code.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
-int main(){
+int main() {
 
-int num1,num2;
-scanf("%d %d", &num1, &num2);
-if(num1%num2==0){
-    printf("Divisible");
-}else{
-    printf("Not divisible");
-}
+    int num1, num2;
+    scanf("%d %d", &num1, &num2);
+
+    // Prevent division by zero
+    if (num2 == 0) {
+        printf("Error: Division by zero is not allowed");
+    } else if (num1 % num2 == 0) {
+        printf("Divisible");
+    } else {
+        printf("Not Divisible");
+    }
 
     return 0;
 }
